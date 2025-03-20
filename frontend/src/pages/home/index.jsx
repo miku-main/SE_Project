c0746631424518dfcdebcc1425e3d23e4f0e5e4f
alad2121 <45607789+alad2121@users.noreply.github.com>
2025-03-20 14:29:35 -0400

    Changed the file system format.

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Home = () => {
+    return (
+        <Box>
+            <Typography>hi</Typography>
+        </Box>
+    )
+}
+
+export default Home;
