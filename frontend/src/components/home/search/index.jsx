d4ef7d5c70f3ae61497ceb6d0ed76fca2f7ed9f7
alad2121 <45607789+alad2121@users.noreply.github.com>
2025-03-22 15:02:23 -0400

    Created a search bar.

---
+++ frontend/src/components/home/search/index.jsx
@@ -0,0 +1,41 @@
+import SearchIcon from "./assets/searchIcon";
+import {InputBase, Box} from "@mui/material";
+
+const Search = () => {
+    const searchContainerStyle = {
+        width:"40rem",
+        backgroundColor:"#FFFFFF",
+        borderRadius:"1.5rem",
+        padding:"2%"
+    };
+
+    const searchInnerContainerStyle = {
+        paddingLeft:"3%",
+        display:"flex",
+        width:"inherit",
+        paddingRight:"6%",
+    };
+
+    const searchInputContainerStyle = {
+        width:"inherit"
+    }
+
+    const searchIconContainerStyle={
+        marginRight:"2%"
+    }
+
+    return(
+        <Box sx={searchContainerStyle}>
+            <Box sx={searchInnerContainerStyle}>
+                <Box sx={searchIconContainerStyle}>
+                    <SearchIcon/>
+                </Box>
+                <Box sx={searchInputContainerStyle}>
+                    <InputBase fullWidth={true} sx={{ml:1,flex:1}} inputProps={{"aria-label": "Search"}}/>
+                </Box>
+            </Box>
+        </Box>
+    )
+}  
+
+export default Search;
