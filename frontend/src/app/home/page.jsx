45349ea7107eef02ed8afc74af2ca36ed2d501da
alad2121 <45607789+alad2121@users.noreply.github.com>
2025-03-24 11:05:17 -0400

    Created the post layout and a switch.

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -1,12 +1,47 @@
-import { Box, Typography } from "@mui/material";
+"use client"
+
+import { Box, Grid2, Switch, Typography, Button } from "@mui/material";
 import Search from "../../components/home/search";
+import Post from "../../components/home/post";
+import { useState } from "react";
 
 const Home = () => {
 
+    const [followingActive, setFollowingActive] = useState(false)
+
     return (
-        <Box>
-            <Box sx={{marginTop:"1rem"}}>
-                <Search/>
+        <Box sx={{height:"inherit"}}>
+
+            <Box sx={{display:"flex", alignItems:"center", flexDirection:"column"}}>
+                <Box sx={{marginTop:"1rem", marginBottom:"2rem"}}>
+                    <Search/>
+                </Box>
+
+                <Box sx={{width:"100%", marginBottom:"1rem"}}>
+                    <Box sx={{display:"flex", float:"right", marginRight:"2%"}}>
+                        <Button onClick={() => {
+                            setFollowingActive(true);
+                        }} sx={{color:"white", backgroundColor: followingActive ? "#FAADAD" : "rgba(250,173,173,0.5)", borderTopRightRadius:"0", borderBottomRightRadius:"0"}}>Following</Button>
+                        <Button onClick={() => {
+                            setFollowingActive(false);
+                        }} sx={{color:"white", backgroundColor: followingActive ? "rgba(250,173,173,0.5)" :"#FAADAD" ,borderTopLeftRadius:"0", borderBottomLeftRadius:"0"}}>Global</Button>
+                    </Box>
+                 
+                </Box>
+
+                <Box sx={{flexGrow:1}}>
+                    <Grid2 container  direction={"row"}  columns={{ xs: 4, sm: 8, md: 12 }}>
+                        {Array(6).fill(1).map(() => {
+                            return (
+                                <Grid2  item size={{ xs: 2, sm: 4, md: 4 }}>
+                                    <Box sx={{margin:"auto", width:"fit-content"}}>
+                                        <Post description={"sadsadsadsadsadsadsadsadsadsadsadsafdsfsadsadsafdsafsadsa"} username={"Username"}/>
+                                    </Box>
+                                </Grid2>
+                            )
+                        })}
+                    </Grid2>
+                </Box>
             </Box>
         </Box>
     )
