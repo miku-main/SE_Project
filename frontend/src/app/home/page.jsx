9742b5e06258fd11f96d32fc3eb9045c1366d933
alad2121 <45607789+alad2121@users.noreply.github.com>
2025-03-25 14:36:12 -0400

    Added dropdowns into the home page.

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -4,18 +4,40 @@ import { Box, Grid2, Switch, Typography, Button } from "@mui/material";
 import Search from "../../components/home/search";
 import Post from "../../components/home/post";
 import { useState } from "react";
-
+import Dropdown from "../../components/global/dropdown";
+import {listOfCountries, listOfIngredients} from "../../constants";
 const Home = () => {
 
     const [followingActive, setFollowingActive] = useState(false)
 
+    const [selectedCountryList, setSelectedCountryList] = useState([]);
+    const [selectedIngredientList, setSelectedIngredientList] = useState([]);
+
+
+    const handleChange = (type,data) => {
+        if(type === "country"){
+            setSelectedCountryList(data);
+        }
+        else if(type === "ingredient"){
+            setSelectedIngredientList(data);
+        }
+    }
     return (
         <Box sx={{height:"inherit"}}>
             <Box sx={{display:"flex", alignItems:"center", flexDirection:"column"}}>
-                <Box sx={{marginTop:"1rem", marginBottom:"2rem"}}>
-                    <Search/>
+                <Box sx={{width:"fit-content", marginBottom:"1%",marginTop:"2%"}}>
+                    <Box sx={{marginBottom:"2%"}}>
+                        <Search/>
+                    </Box>
+                    <Box sx={{display:"flex", justifyContent:"space-between", float:"left"}}>
+                        <Box sx={{marginRight:"10%"}}>
+                            <Dropdown handleChange={(event) => handleChange("country",event.target.value)} selectedValue={selectedCountryList} isMultiple={true} placeholder={"Countries"} data={listOfCountries}/>
+                        </Box>
+                        <Box>
+                            <Dropdown handleChange={(event) => handleChange("ingredient", event.target.value)} selectedValue={selectedIngredientList} isMultiple={true} placeholder={"Ingredients"} data={listOfIngredients}/>
+                        </Box>
+                    </Box>
                 </Box>
-
                 <Box sx={{width:"100%", marginBottom:"1rem"}}>
                     <Box sx={{display:"flex", float:"right", marginRight:"2%"}}>
                         <Button onClick={() => {
@@ -25,7 +47,6 @@ const Home = () => {
                             setFollowingActive(false);
                         }} sx={{color:"white", backgroundColor: followingActive ? "rgba(250,173,173,0.5)" :"#FAADAD" ,borderTopLeftRadius:"0", borderBottomLeftRadius:"0"}}>Global</Button>
                     </Box>
-                 
                 </Box>
 
                 <Box sx={{flexGrow:1}}>
