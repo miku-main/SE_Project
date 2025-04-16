6a8c875f7d9036721977bcb4df327a24a4a162c6
alad2121 <45607789+alad2121@users.noreply.github.com>
2025-04-16 16:05:52 -0400

    Update page.jsx

--- frontend/src/app/bookmark/page.jsx
+++ frontend/src/app/bookmark/page.jsx
@@ -1,15 +1,13 @@
+"use client"
+
 import { useState } from "react";
 import { Box, Grid, Typography, BottomNavigation, BottomNavigationAction, Card, CardMedia, CardContent } from "@mui/material";
-import Home from "../../components/home/home";
-import Search from "../../components/home/search";
-import Post from "../../components/home/post";
-import Dropdown from "../../components/global/dropdown";
+
 import HomeIcon from '@mui/icons-material/Home';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import GroupIcon from '@mui/icons-material/Group';
-import {listOfCountries, listOfIngredients} from "../../constants";
 
 const Bookmark = () => {
         const [savedData, setSavedData] = useState([
@@ -48,13 +46,6 @@ const Bookmark = () => {
                     ))}
                 </Grid>
             </Box>
-            <BottomNavigation showLabels sx={{ boxShadow: '0 -2px 5px rgba(0,0,0,0.1)' }}>
-                <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-                <BottomNavigationAction label="Post" icon={<AddBoxIcon />} />
-                <BottomNavigationAction label="Bookmarks" icon={<BookmarkIcon />} />
-                <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />} />
-                <BottomNavigationAction label="Groups" icon={<GroupIcon />} />
-            </BottomNavigation>
         </Box>
     )
 }
